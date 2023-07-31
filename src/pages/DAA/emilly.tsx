c89b46c782d4e419612026443fa43b9b6819fc80
MachareteL <macharetelucas@gmail.com>
2023-07-31 15:39:33 -0300

    fix emilly page

--- src/pages/DAA/emilly.tsx
+++ src/pages/DAA/emilly.tsx
@@ -1,20 +1,21 @@
-import { NextPage } from "next";
-import PersonalProfile from "../components/PersonalProfile";
-import Card from "../components/Card";
+import { NextPage } from 'next'
+import PersonalProfile from '../components/PersonalProfile';
+import Card from '../components/Card';
 
 interface Props {}
 
 const Emilly: NextPage<Props> = ({}) => {
+
   const labels: Label[] = [
     {
       title: "Contato",
       icons: [
         {
-          icon: "https://cdn-icons-png.flaticon.com/512/1384/1384014.png",
+          icon: "https://www.flaticon.com/br/icone-gratis/linkedin_145807?term=linkedin&page=1&position=2&origin=tag&related_id=145807",
           href: "https://www.linkedin.com/in/emillysug/",
         },
         {
-          icon: "https://cdn-icons-png.flaticon.com/512/733/733609.png",
+          icon: "https://www.flaticon.com/br/icone-gratis/github_733609?term=github&page=1&position=3&origin=search&related_id=733609",
           href: "https://github.com/Emillyzinha",
         },
         {
@@ -37,7 +38,7 @@ const Emilly: NextPage<Props> = ({}) => {
       ],
     },
     {
-      title: "Bibliotecas",
+      title: "Tenologias",
       icons: [
         {
           icon: "https://img2.gratispng.com/20180817/ysc/kisspng-logo-scikit-learn-python-github-portable-network-g-go-to-image-page-5b77997b36f4f5.3677823415345647312251.jpg",
@@ -48,24 +49,28 @@ const Emilly: NextPage<Props> = ({}) => {
           href: "https://openpyxl.readthedocs.io/en/stable/",
         },
         {
-          icon: "https://me315-unicamp.github.io/aulas/imgs_aula11/pandas.png",
-          href: "https://pandas.pydata.org/",
+          icon: "https://imgtr.ee/images/2023/07/31/7ceb4c8b45f894c79165ec3cd97b87b5.png",
+          href: "https://www.djangoproject.com//",
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/1260/1260667.png",
+          href: "https://react.dev/",
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/1260/1260667.png",
+          href: "https://react.dev/",
+        },
+        {
+          icon: "https://www.flaticon.com/br/icone-gratis/armazenamento-de-banco-de-dados_5732837?term=banco+de+dados&page=1&position=3&origin=search&related_id=5732837",
+          href: "https://www.mysql.com/",
         },
       ],
     },
   ];
 
-  const projetos = [
-    "Apprentice Management",
-    "CorporateWiki",
-    "Bancada Móvel para Eventos Híbridos",
-  ];
+  const projetos = ["Apprentice Management", "CorporateWiki", "Bancada Móvel para Eventos Híbridos"];
   const curiosities = ["Já tentei escrever um livro", "Psicóloga infantil"];
-  const formation = [
-    "E.E. Carlos Gomes",
-    "SENAI",
-    "Unimetrocamp - Ciência da Computação",
-  ];
+  const formation = ["E.E. Carlos Gomes", "Curso Administrativo - Patrulheiros",  "SENAI", "Ciência da Computação - Unimetrocamp"];
   const hobbies = ["Ler", "Assistir sitcom", "Cozinhar"];
 
   return (
@@ -73,14 +78,16 @@ const Emilly: NextPage<Props> = ({}) => {
       image="https://github.com/Emillyzinha.png"
       description="Tenho 18 anos e moro em Campinas"
       introduction="Oi! Sou a Emilly Santos"
-      labels={labels}
-    >
-      <Card title="Projetos" list={projetos} />
-      <Card title="Curiosidades" list={curiosities} />
-      <Card title="Formação" list={formation} />
-      <Card title="Hobbies" list={hobbies} />
-    </PersonalProfile>
-  );
-};
+      labels={labels}>
 
-export default Emilly;
+        <Card title="Projetos" list={projetos}/>
+        <Card title="Curiosidades" list={curiosities}/>
+        <Card title="Formação" list={formation}/>
+        <Card title="Hobbies" list={hobbies}/>
+        
+      </PersonalProfile>
+
+  )
+}
+
+export default Emilly
